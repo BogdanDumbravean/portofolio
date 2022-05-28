73f57f594daec9af768090f7db7efbb34a37a9b7
Bogdan Dumbravean <dumbraveanb@yahoo.com>
2022-05-28 20:18:23 +0200

    Added information about each project, solved some bugs and uploaded online

--- src/app/project.list.ts
+++ src/app/project.list.ts
@@ -3,8 +3,8 @@ import { ImgPath, Project, Text } from "./project.model";
 export const PROGRAMMING: Project[] = [
     new Project(
         "Conference Management",
-        "assets/img/facebook.png",
-        "I had most of my education in Romania. My highschool and bachelor’s were in the domain of computer science. Doing a master's in the Netherlands on game technology.",
+        "assets/img/Faculta/ISS/Login.png",
+        "Application for managing the stages of a conference, made in a team. I was chosen as team leader, so I had knowledge of the whole application, but my main tasks were in the back-end and the database.",
         [
             new ImgPath("assets/img/facebook.png"),
             new Text("a"),
@@ -13,9 +13,21 @@ export const PROGRAMMING: Project[] = [
         ]
     ),
     new Project(
+        "Bolts and Gears",
+        "assets/img/Liceu/FTC.png",
+        "First season of FTC in Romania. The contest starts from gathering funds and resources, to building the robot, and ends with a competition between the robots of the qualified teams. We qualified to the national phase. My main task was programming, but I contributed a lot to building the physical part too.",
+        [
+            new Text("https://www.facebook.com/boltsngears"),
+            new ImgPath("assets/img/linkedin.png"),
+            new Text("b2"),
+            new Text("c"),
+            new ImgPath("assets/img/facebook.png")
+        ]
+    ),
+    new Project(
         "University Job Formular",
-        "assets/img/linkedin.png",
-        "The activity I appreciate the most is doing sports. Other than that, I also occupy my time with reading, watching shows, playing games, doing something artistic, and travelling.",
+        "assets/img/Practica/Screenshot 2022-03-01 221720.png",
+        "As practical work, we had to develop websites for the university. Our team had the task of creating a website for teachers that apply to the university to complete a formular and upload their documents. Worked in the front-end, back-end, and database, however I was the representative of the backend team.",
         [
             new Text("a2"),
             new ImgPath("assets/img/linkedin.png"),
@@ -26,8 +38,8 @@ export const PROGRAMMING: Project[] = [
     ),
     new Project(
         "Activities Management",
-        "assets/img/linkedin.png",
-        "The activity I appreciate the most is doing sports. Other than that, I also occupy my time with reading, watching shows, playing games, doing something artistic, and travelling.",
+        "assets/img/Faculta/MA/Images/Looks/List_Offline.png",
+        "Mobile application done individually for a course. Used for organising daily activities quickly. Realised in both flutter and kotlin. Works with a server database when connected to the internet, and saves data locally for offline interaction.",
         [
             new Text("a2"),
             new ImgPath("assets/img/linkedin.png"),
@@ -38,8 +50,8 @@ export const PROGRAMMING: Project[] = [
     ),
     new Project(
         "Automated Stock Information",
-        "assets/img/linkedin.png",
-        "The activity I appreciate the most is doing sports. Other than that, I also occupy my time with reading, watching shows, playing games, doing something artistic, and travelling.",
+        "assets/img/Faculta/RPA/Screenshot 2022-03-01 222341.png",
+        "Team project done in UiPath for automated screen capture from stock information websites. The application can recieve emails with the stocks' identifiers, after which it calculates if the stock is on an upward trend, and it sends a reply with the result, as well as other important information.",
         [
             new Text("a2"),
             new ImgPath("assets/img/linkedin.png"),
@@ -50,8 +62,8 @@ export const PROGRAMMING: Project[] = [
     ),
     new Project(
         "Intelligent Crowd Behaviour",
-        "assets/img/linkedin.png",
-        "The activity I appreciate the most is doing sports. Other than that, I also occupy my time with reading, watching shows, playing games, doing something artistic, and travelling.",
+        "assets/img/Faculta/Licenta/Screenshot 2022-05-28 121938.png",
+        "A 3D simulation of a stadium evacuation done in Unity. The scenarios are done following real life planning, with a combination of behaviours for the AI agents.",
         [
             new Text("a2"),
             new ImgPath("assets/img/linkedin.png"),
@@ -62,8 +74,8 @@ export const PROGRAMMING: Project[] = [
     ),
     new Project(
         "Movie Rental",
-        "assets/img/linkedin.png",
-        "The activity I appreciate the most is doing sports. Other than that, I also occupy my time with reading, watching shows, playing games, doing something artistic, and travelling.",
+        "assets/img/Faculta/MPP/MPP.png",
+        "Web application for managing the rental of movies realised in a two-man team. Front-end done in Angular, back-end in Java, and different versions for different databases.",
         [
             new Text("a2"),
             new ImgPath("assets/img/linkedin.png"),
@@ -74,8 +86,8 @@ export const PROGRAMMING: Project[] = [
     ),
     new Project(
         "Raytracer",
-        "assets/img/linkedin.png",
-        "The activity I appreciate the most is doing sports. Other than that, I also occupy my time with reading, watching shows, playing games, doing something artistic, and travelling.",
+        "assets/img/Faculta/VR/Screenshot 2022-03-01 222923.png",
+        "A physics application of rays and reflections for rendering a simple 3D scene. Multiple images are created from different angles of the scene. Project realised in C#.",
         [
             new Text("a2"),
             new ImgPath("assets/img/linkedin.png"),
@@ -86,8 +98,8 @@ export const PROGRAMMING: Project[] = [
     ),
     new Project(
         "Acrophobia Helper",
-        "assets/img/linkedin.png",
-        "The activity I appreciate the most is doing sports. Other than that, I also occupy my time with reading, watching shows, playing games, doing something artistic, and travelling.",
+        "assets/img/Faculta/VR/Screenshot 2022-03-01 225758.png",
+        "Unity project with the purpose of helping people suffering from fear of heights. The user can use the virtual environment to gradually ascend on a board, at their own pace.",
         [
             new Text("a2"),
             new ImgPath("assets/img/linkedin.png"),
@@ -98,8 +110,8 @@ export const PROGRAMMING: Project[] = [
     ),
     new Project(
         "Slideshow",
-        "assets/img/linkedin.png",
-        "The activity I appreciate the most is doing sports. Other than that, I also occupy my time with reading, watching shows, playing games, doing something artistic, and travelling.",
+        "assets/img/Faculta/Web/Screenshot 2022-03-01 214921.png",
+        "Simple web application displaying a slideshow. Small project made for further stabilising my basics of web development.",
         [
             new Text("a2"),
             new ImgPath("assets/img/linkedin.png"),
@@ -110,8 +122,8 @@ export const PROGRAMMING: Project[] = [
     ),
     new Project(
         "Informative Fill and Lee Algorithms",
-        "assets/img/linkedin.png",
-        "The activity I appreciate the most is doing sports. Other than that, I also occupy my time with reading, watching shows, playing games, doing something artistic, and travelling.",
+        "assets/img/Liceu/Atestat/Screenshot 2022-03-01 225303.png",
+        "Visual C# application made in highschool explaining two algorithms: the Fill and the Lee. Apart from theory that explains how and when to use the algorithm, specific problems are presented with a step by step animation for each algorithm.",
         [
             new Text("a2"),
             new ImgPath("assets/img/linkedin.png"),
@@ -121,9 +133,9 @@ export const PROGRAMMING: Project[] = [
         ]
     ),
     new Project(
-        "Biology Quizz",
-        "assets/img/linkedin.png",
-        "The activity I appreciate the most is doing sports. Other than that, I also occupy my time with reading, watching shows, playing games, doing something artistic, and travelling.",
+        "Biology Quiz",
+        "assets/img/Liceu/appbio.png",
+        "Helpful quiz for students that want to test their biology skills, done in highschool. The questions are imported from previous years of the \"Bacalaureat\" exam from Romania. Presents a test with multiple-choice questions.",
         [
             new Text("a2"),
             new ImgPath("assets/img/linkedin.png"),
@@ -134,8 +146,8 @@ export const PROGRAMMING: Project[] = [
     ),
     new Project(
         "Mini Paint",
-        "assets/img/linkedin.png",
-        "The activity I appreciate the most is doing sports. Other than that, I also occupy my time with reading, watching shows, playing games, doing something artistic, and travelling.",
+        "assets/img/Liceu/paint.png",
+        "A basic recreation of paint from my highschool days. Has a number of features of the paint application, including undo and redo.",
         [
             new Text("a2"),
             new ImgPath("assets/img/linkedin.png"),
@@ -149,8 +161,8 @@ export const PROGRAMMING: Project[] = [
 export const GAMES: Project[] = [
     new Project(
         "Carl",
-        "assets/img/linkedin.png",
-        "The activity I appreciate the most is doing sports. Other than that, I also occupy my time with reading, watching shows, playing games, doing something artistic, and travelling.",
+        "assets/img/Games/Carl.png",
+        "Winner of the GetCrEAtive hackathon, this game was done in a three-man team in 24h. It's a side-view 2D racing game, but with a twist. I'll let you see for yourself 😉",
         [
             new Text("a2"),
             new ImgPath("assets/img/linkedin.png"),
@@ -161,30 +173,8 @@ export const GAMES: Project[] = [
     ),
     new Project(
         "Little Knight",
-        "assets/img/facebook.png",
-        "I had most of my education in Romania. My highschool and bachelor’s were in the domain of computer science. Doing a master's in the Netherlands on game technology.",
-        [
-            new ImgPath("assets/img/facebook.png"),
-            new Text("a"),
-            new Text("b"),
-            new ImgPath("assets/img/linkedin.png")
-        ]
-    ),
-    new Project(
-        "Book of Heroes",
-        "assets/img/facebook.png",
-        "I had most of my education in Romania. My highschool and bachelor’s were in the domain of computer science. Doing a master's in the Netherlands on game technology.",
-        [
-            new ImgPath("assets/img/facebook.png"),
-            new Text("a"),
-            new Text("b"),
-            new ImgPath("assets/img/linkedin.png")
-        ]
-    ),
-    new Project(
-        "Board of Heroes",
-        "assets/img/facebook.png",
-        "I had most of my education in Romania. My highschool and bachelor’s were in the domain of computer science. Doing a master's in the Netherlands on game technology.",
+        "assets/img/Games/LK/Screenshot_20190701-213821.png",
+        "My first released game, for android devices. Side-view 2D fantasy game with a knight straded on a flying island. The game is not on Google Play anymore because I haven't updated it after new rules have been enforced regarding ads",
         [
             new ImgPath("assets/img/facebook.png"),
             new Text("a"),
@@ -194,8 +184,30 @@ export const GAMES: Project[] = [
     ),
     new Project(
         "Procedural Village Generation",
-        "assets/img/facebook.png",
-        "I had most of my education in Romania. My highschool and bachelor’s were in the domain of computer science. Doing a master's in the Netherlands on game technology.",
+        "assets/img/Faculta/Master/unnamed.png",
+        "Although not a game by itself, this project is intended for game development. It's a gamified approach on village procedural generation, making use of suggestions to let the designer maintain more control over the creation process.",
+        [
+            new ImgPath("assets/img/facebook.png"),
+            new Text("a"),
+            new Text("b"),
+            new ImgPath("assets/img/linkedin.png")
+        ]
+    ),
+    new Project(
+        "Book of Heroes",
+        "assets/img/Games/BookOfHeroes/Book of Heroes.png",
+        "Team-made game in 3D, featuring the jourey of a mage that fights to save his city from monsters. With this game, we won an award at a national competition.",
+        [
+            new ImgPath("assets/img/facebook.png"),
+            new Text("a"),
+            new Text("b"),
+            new ImgPath("assets/img/linkedin.png")
+        ]
+    ),
+    new Project(
+        "Board of Heroes",
+        "assets/img/Games/IMG-20200308-WA0000.jpg",
+        "A game started with a team, that has sadly been left unfinished. It's a different take on auto-chess games, realised in 3D.",
         [
             new ImgPath("assets/img/facebook.png"),
             new Text("a"),
@@ -205,8 +217,8 @@ export const GAMES: Project[] = [
     ),
     new Project(
         "Flying Jousting",
-        "assets/img/facebook.png",
-        "I had most of my education in Romania. My highschool and bachelor’s were in the domain of computer science. Doing a master's in the Netherlands on game technology.",
+        "assets/img/Games/Flying Jousting/Screenshot 2022-05-28 124705.png",
+        "Imagine jousting, but in 3D. A multiplayer game where you fly on your horse to defeat your opponents, the arena being the inside of a cube. First multiplayer project I made, implementing rooms for players, a scoreboard, and a lot of fun.",
         [
             new ImgPath("assets/img/facebook.png"),
             new Text("a"),
@@ -216,8 +228,8 @@ export const GAMES: Project[] = [
     ),
     new Project(
         "Friends",
-        "assets/img/facebook.png",
-        "I had most of my education in Romania. My highschool and bachelor’s were in the domain of computer science. Doing a master's in the Netherlands on game technology.",
+        "assets/img/Games/FriendsGame/Screenshot 2022-05-28 130558.png",
+        "A fun little project that has as main characters me and my friends. The time is set during our high school days, and has us fighting with all the disciplines.",
         [
             new ImgPath("assets/img/facebook.png"),
             new Text("a"),
@@ -225,4 +237,16 @@ export const GAMES: Project[] = [
             new ImgPath("assets/img/linkedin.png")
         ]
     )
+    // ,
+    // new Project(
+    //     "Vandals",
+    //     "",
+    //     "",
+    //     [
+    //         new ImgPath("assets/img/facebook.png"),
+    //         new Text("a"),
+    //         new Text("b"),
+    //         new ImgPath("assets/img/linkedin.png")
+    //     ]
+    // )
 ]
